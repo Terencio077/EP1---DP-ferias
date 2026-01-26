706ac65e3f6c8e3f46f4f75213e38e5d4b230964
Terencio077 <guilhermetm2@al.insper.edu.br>
2026-01-26 18:56:23 -0300

    mudando pra ficar igual o enunciado

--- funcoes.py
+++ funcoes.py
@@ -6,10 +6,9 @@ def cria_pecas ():
     for i in range(7):
         for j in range(i+1):
             peca = []
-            peca.append(i)
             peca.append(j)
-            if peca not in pecas:
-                pecas.append(peca)
+            peca.append(i)
+            pecas.append(peca)
            
     shuffle(pecas)
     return pecas
