c5a5cbed1e438fc518752e6ba625d8592557623a
Terencio077 <guilhermetm2@al.insper.edu.br>
2026-01-26 22:37:57 -0300

    acho que foi

--- funcoes.py
+++ funcoes.py
@@ -70,16 +70,7 @@ def adiciona_na_mesa(peca_add,mesa):
         inicio = mesa[0][0]
         fim = mesa[len(mesa)-1][1]
 
-        if peca_add[0] == fim:
-            mesa.append(peca_add)
-            mesa_final = mesa
-
-        elif peca_add[1] == fim:
-            peca = [peca_add[1],peca_add[0]]
-            mesa.append(peca)
-            mesa_final = mesa 
-            
-        elif peca_add[1] == inicio:
+        if peca_add[1] == inicio:
             mesa_final.append(peca_add)
             for peca in mesa:
                 mesa_final.append(peca)
@@ -88,6 +79,17 @@ def adiciona_na_mesa(peca_add,mesa):
             mesa_final.append(peca)
             for coisa in mesa:
                 mesa_final.append(coisa)
+        elif peca_add[0] == fim:
+            mesa.append(peca_add)
+            mesa_final = mesa
+
+        elif peca_add[1] == fim:
+            peca = [peca_add[1],peca_add[0]]
+            mesa.append(peca)
+            mesa_final = mesa 
+            
+      
+
         
 
     return mesa_final
